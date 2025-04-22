817f31e5cc6a326021f8cfe0543651dcd4163126
Andres Bravo <bravoandres1706@gmail.com>
2025-04-22 08:51:42 -0400

    la funcion

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 console.log("este es el cambio que vamos a hacer")
 console.log("este es otro cambio 2")
 
-console.log("este cambio marcara un error ")
+const funcionMal = (x) => {
+    console.log("este no funciona",x)
+}
+
+funcionMal("alex")
