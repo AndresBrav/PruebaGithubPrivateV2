d3dc2c1a2bb1fbbe39fab27e7c31bd43a627e3cb
Andres Bravo <bravoandres1706@gmail.com>
2025-04-22 09:50:26 -0400

    importando suma

---
+++ funciones.js
@@ -0,0 +1,7 @@
+const suma = (numero1, numero2) => {
+    console.log("la suma es", numero1 + numero2)
+};
+
+module.exports = {
+    suma
+};
--- index.js
+++ index.js
@@ -4,4 +4,4 @@ console.log("este es el cambio que vamos a hacer");
 console.log("este es otro cambio 2");
 
 console.log("este cambio marcará un error (no debería ahora)");
-// suma(1,2)
+suma(1,2)
