1bac40c2be6284d74dbd96eb69d33048b8e81046
Andres Bravo <bravoandres1706@gmail.com>
2025-04-22 08:54:56 -0400

    Revert "la funcion"
    This reverts commit 817f31e5cc6a326021f8cfe0543651dcd4163126.

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 console.log("este es el cambio que vamos a hacer")
 console.log("este es otro cambio 2")
 
-const funcionMal = (x) => {
-    console.log("este no funciona",x)
-}
-
-funcionMal("alex")
+console.log("este cambio marcara un error ")
