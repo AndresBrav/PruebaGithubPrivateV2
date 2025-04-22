d78304f09269f88d75f06d2943175be021cdbf21
Andres Bravo <bravoandres1706@gmail.com>
2025-04-22 09:41:56 -0400

    mal

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-console.log("este es el cambio que vamos a hacer")
-console.log("este es otro cambio 2")
+const { suma } = require('./funciones'); // sin extensión .js, así está bien para CommonJS
 
-console.log("este cambio marcara un error ")
+console.log("este es el cambio que vamos a hacer");
+console.log("este es otro cambio 2");
+
+console.log("este cambio marcará un error (no debería ahora)");
+// suma(1,2)
