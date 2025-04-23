f819ccf05ef39a685169ecb7ad89c3260e032feb
Andres Bravo <bravoandres1706@gmail.com>
2025-04-23 09:28:48 -0400

    realizando despedidas

---
+++ despedidas.js
@@ -0,0 +1,9 @@
+const adios = () => {
+    console.log("hasta luego nos vemos luego")
+}
+
+const despedida = () => {
+    console.log("hasta siempre ...........")
+}
+
+module.exports = {adios,despedida}
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { adios, despedida } = require('./despedidas');
 const { suma,resta,multiplicacion, division } = require('./funciones'); // sin extensión .js, así está bien para CommonJS
 
 console.log("vamos a ver la suma");
@@ -9,3 +10,5 @@ multiplicacion(9,9)
 console.log("vamos a ver la division")
 division(8,4)
 
+adios()
+despedida()
