3f6ab9b3ea9de4a79be8919f705b0905fe3366f9
Andres Bravo <bravoandres1706@gmail.com>
2025-04-24 10:59:29 -0400

    6 commit

--- funcionesAdicionales.js
+++ funcionesAdicionales.js
@@ -2,6 +2,9 @@ const adicionales = () => {
     console.log("1 commit")
     console.log("2 commit")
     console.log("3 commit")
+    console.log("4 commit")
+    console.log("5 commit")
+    console.log("6 commit")
 }
 
 module.exports ={adicionales}
