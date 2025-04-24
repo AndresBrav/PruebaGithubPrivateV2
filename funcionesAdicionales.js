912f16b64ead11a12e3e295a6d9b42a5370f60b7
Andres Bravo <bravoandres1706@gmail.com>
2025-04-24 10:15:03 -0400

    1 commit

---
+++ funcionesAdicionales.js
@@ -0,0 +1,5 @@
+const adicionales = () => {
+    console.log("1 commit")
+}
+
+module.exports ={adicionales}
--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 const { adios, despedida } = require('./despedidas');
 const { suma,resta,multiplicacion, division } = require('./funciones'); // sin extensión .js, así está bien para CommonJS
+const { adicionales } = require('./funcionesAdicionales');
 const {hola,estado, bienvenida, bienvenida2} = require('./saludos');
 const { bienvenida22, hola2, estado2, bienvenida21 } = require('./saludos2');
 
-console.log("vamos a ver la suma");
-suma(1,2)
-console.log("vamos a ver la resta")
-resta(8,2)
-console.log("vamos a ver la multiplicacion")
-multiplicacion(9,9)
-console.log("vamos a ver la division")
-division(8,4)
+adicionales()
 
-hola()
-estado()
-bienvenida()
-bienvenida2()
+// console.log("vamos a ver la suma");
+// suma(1,2)
+// console.log("vamos a ver la resta")
+// resta(8,2)
+// console.log("vamos a ver la multiplicacion")
+// multiplicacion(9,9)
+// console.log("vamos a ver la division")
+// division(8,4)
 
-console.log("veremos otros saludos de la otra rama *22222222222")
-hola2()
-estado2()
-bienvenida21()
-bienvenida22()
+// hola()
+// estado()
+// bienvenida()
+// bienvenida2()
 
-adios()
-despedida()
+// console.log("veremos otros saludos de la otra rama *22222222222")
+// hola2()
+// estado2()
+// bienvenida21()
+// bienvenida22()
+
+// adios()
+// despedida()
 
 
 
