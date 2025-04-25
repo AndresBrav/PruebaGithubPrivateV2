67d904d4b981fbad48db2d82033d19d8e8a127e1
Andres Bravo <bravoandres1706@gmail.com>
2025-04-25 11:35:56 -0400

    arreglamos la resta

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { resta } = require("./operaciones/resta")
 const { suma } = require("./operaciones/suma")
-
+// agregamos la resta 
 console.log("la suma es: ",suma(7,8))
 console.log("la resta es: ",resta(9,8))
