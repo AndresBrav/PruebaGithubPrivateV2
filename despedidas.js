c8e4a45c346a7b6d132b1d1ba8a2bb922941bd57
Andres Bravo <bravoandres1706@gmail.com>
2025-04-25 09:18:41 -0400

    vamos a organizar mejor

--- despedidas.js
+++
@@ -1,9 +0,0 @@
-const adios = () => {
-    console.log("hasta luego nos vemos luego")
-}
-
-const despedida = () => {
-    console.log("hasta siempre ...........")
-}
-
-module.exports = {adios,despedida}
