f2d0b84ef77927d8438cae25f8efbbd59f5f67ce
Andres Bravo <bravoandres1706@gmail.com>
2025-04-25 10:09:33 -0400

    suma aniadida

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-console.log("primer inicio")
+const { suma } = require("./operaciones/suma")
+
+console.log("la suma es: ",suma(7,8))
---
+++ operaciones/suma.js
@@ -0,0 +1,7 @@
+const suma = (numero1, numero2) => {
+    return numero1, numero2
+}
+
+module.exports = {
+    suma
+}
