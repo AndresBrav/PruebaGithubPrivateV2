8388d30e262bb4d37ff293322cce22c46e7fdaa3
Andres Bravo <bravoandres1706@gmail.com>
2025-04-25 10:55:37 -0400

    multiplicacion aniadida

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { multi } = require("./operaciones/multiplicacion")
 const { suma } = require("./operaciones/suma")
 
 console.log("la suma es: ",suma(7,8))
+console.log("la multiplicacion es ",multi(7,9))
