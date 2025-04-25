3b7a92012b7c1102091e156578ad3ad5bb595bc3
Andres Bravo <bravoandres1706@gmail.com>
2025-04-25 11:08:13 -0400

    division aniadida

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { divi } = require("./operaciones/division")
 const { suma } = require("./operaciones/suma")
 
 console.log("la suma es: ",suma(7,8))
+console.log("la division es ",divi(10,2))
