ab8fd6d3643618aa4870be24837c42ad3e5673fa
Andres Bravo <bravoandres1706@gmail.com>
2025-04-28 08:51:20 -0400

    raiz cuadrada

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const { suma } = require("./operaciones/suma")
 const { resta } = require("./operaciones/resta")
 const { multi } = require("./operaciones/multiplicacion")
 const { divi } = require("./operaciones/division")
+const { raiz } = require("./operaciones2/raiz")
 //arreglamos los conflictos
 // agregamos la resta 
 console.log("la suma es: ",suma(7,8))
 console.log("la resta es: ",resta(9,8))
 console.log("la multiplicacion es ",multi(7,9))
 console.log("la division es ",divi(10,2))
+console.log()
+//operaciones 2
+console.log("la raiz cuadrada es ",raiz(9))
 
