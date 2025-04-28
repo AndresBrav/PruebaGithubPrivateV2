398ac191f88b2287ae25143ba165f23777643092
Andres Bravo <bravoandres1706@gmail.com>
2025-04-28 11:52:12 -0400

    redondeo

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { raiz } = require("./operaciones2/raiz")
 const { potencia } = require("./operaciones2/potencia")
 const { logaritmo } = require("./operaciones2/logaritmoNatural")
 const {logB2} = require('./operaciones2/logaritmoBase2')
+const { redondearNumero } = require("./operaciones3/redondeo")
 //arreglamos los conflictos
 // agregamos la resta 
 console.log("la suma es: ",suma(7,8))
@@ -19,4 +20,7 @@ console.log("la raiz cuadrada es ",raiz(9))
 console.log("la potencia es",potencia(6,2))
 console.log("el logaritmo natural es",logaritmo(10))
 console.log("el logaritmo de base 2 es",logB2(8))
+
+//operaciones 3
+console.log("el redondeo es ",redondearNumero(81.6))
 
